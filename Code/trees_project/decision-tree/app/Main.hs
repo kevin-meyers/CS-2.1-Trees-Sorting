115e6715b34e06bb0cc08202a5cfcf347ea3b1fb
Kevin <kevinm1776@gmail.com>
2020-05-03 17:22:04 -0400

    Recursively builds tree.

--- Code/trees_project/decision-tree/app/Main.hs
+++ Code/trees_project/decision-tree/app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import Lib
 import Utils
 
+import qualified Data.MultiSet as MultiSet
+
 main :: IO ()
 main = putStrLn "Please"
 
@@ -21,11 +23,15 @@ train :: [[Double]] -> [Int] -> DecisionTree Double
 train xs ys = buildTree $ partitionsFrom xs ys
 
 buildTree :: Inputs Double -> DecisionTree Double
-buildTree (xs, ys) = Leaf 5
+buildTree (xs, ys)
+  | size describeY ys < 3 = Leaf (MultiSet.findMax describeY ys)
+  | otherwise = Node (> t) (buildTree xs left) (buildTree xs right)
   where
     (bestFeat, bestIndex) = getBestSplit ys
     (left, right) = childrenData bestIndex ys
     t = midPoint bestIndex (xs !! bestFeat)
+
+describeY = snd . head . head
 
 midPoint :: Int -> [Double] -> Double
 midPoint i ys = (ys !! i + (ys !! (i - 1))) / 2
